a1b7c4a9f9781d32e834795c1c189f35536078bb
tannywebbers <tannywebbers@gmail.com>
2026-08-03 21:22:17 +0100

    Update Playwright installation commands in build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-playwright install chromium
+python -m playwright install chromium
 
-playwright install-deps chromium || true
+python -m playwright install-deps chromium || true
